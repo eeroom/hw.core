b5808d82324ec20619cbacdc4456e050194e56e6
邓常青 <eedroom@163.com>
2021-01-17 11:59:49 +0800

    maven的modul和parent modul,等价于vs中解决方案-项目

---
+++ sln-model/src/main/java/org/azeroth/sln/model/Student.java
@@ -0,0 +1,7 @@
+package org.azeroth.sln.model;
+
+public class Student {
+    public void  Say(String value){
+        System.out.println(value);
+    }
+}
---
+++ sln-ui/src/main/java/org/azeroth/App.java
@@ -0,0 +1,14 @@
+package org.azeroth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        org.azeroth.sln.model.Student st=new org.azeroth.sln.model.Student();
+        st.Say("你好");
+    }
+}
