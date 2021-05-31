5193e1ebc52a9151be73d62c0c2b62cfe35c8a34
邓常青 <eedroom@163.com>
2021-05-31 23:39:09 +0800

    更新依赖

--- axisclient/src/main/java/org/azeroth/axisclient/App.java
+++ axisclient/src/main/java/org/azeroth/axisclient/App.java
@@ -4,14 +4,20 @@ import java.rmi.RemoteException;
 
 public class App {
     public static void main(String[] args) throws Throwable {
+        invokeHelloWorld(44);
+
+    }
+
+    private static void invokeHelloWorld(int a) throws Throwable {
         String endpointURL = "http://localhost:49755/Home.asmx";
 
         org.apache.axis.client.Service service = new org.apache.axis.client.Service();
 
         org.apache.axis.client.Call call = (org.apache.axis.client.Call) service.createCall();
         call.setProperty(org.apache.axis.AxisEngine.PROP_DOMULTIREFS,false);
-        call.setTimeout(60000);
+        //call.setTimeout(60000);
         call.setTargetEndpointAddress(endpointURL);
+        call.setUseSOAPAction(true);
         call.setSOAPActionURI("http://tempuri.org/HelloWorld");
         call.setOperationName(new javax.xml.namespace.QName("http://tempuri.org/","HelloWorld"));// 设置操作的名称。
 
@@ -20,7 +26,7 @@ public class App {
                 org.apache.axis.encoding.XMLType.SOAP_INT,
                 javax.xml.rpc.ParameterMode.IN);// 参数的类型
 
-        String result = (String) call.invoke(new Object[]{44});// 执行调用
+        String result = (String) call.invoke(new Object[]{a});// 执行调用
 
         System.out.println(result);
     }
