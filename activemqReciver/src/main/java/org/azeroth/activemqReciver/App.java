d5b157ec13e0863ae3580c0ce95479216d0ddc53
邓常青 <eedroom@163.com>
2021-07-02 21:24:57 +0800

    activemq，使用事物发送消息

--- activemqReciver/src/main/java/org/azeroth/activemqReciver/App.java
+++ activemqReciver/src/main/java/org/azeroth/activemqReciver/App.java
@@ -4,11 +4,20 @@ import javax.jms.Session;
 
 public class App {
     public static void main(String[] arg) throws Throwable{
+        invoke();
+
+
+    }
+
+    /*
+    接收消息，最简单的新式，不依赖其他类库
+     */
+    private static void invoke()throws Throwable {
         System.out.println("hello world");
-        var fac=new org.apache.activemq.ActiveMQConnectionFactory();
-        fac.setBrokerURL("tcp://localhost:61616");
+        var fac=new org.apache.activemq.ActiveMQConnectionFactory("tcp://localhost:61616");
         var cnn= fac.createConnection();
         cnn.start();
+
         var session= cnn.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
         var target= session.createQueue("udl");
@@ -19,7 +28,5 @@ public class App {
             var msg= (org.apache.activemq.command.ActiveMQTextMessage)reciver.receive();
             System.out.println("收到消息:"+msg.getText());
         }
-
-
     }
 }
--- activemqSender/src/main/java/org/azeroth/activemqSender/App.java
+++ activemqSender/src/main/java/org/azeroth/activemqSender/App.java
@@ -7,20 +7,30 @@ import javax.jms.Session;
 
 public class App {
     public static void main(String[] args) throws Throwable{
+        invoke();
+
+    }
+
+    /*
+    发送消息，最简单的形式，不依赖其他类库
+     */
+    private static void invoke() throws Throwable{
         System.out.println("hello world");
-        var fac=new org.apache.activemq.ActiveMQConnectionFactory();
-        fac.setBrokerURL("tcp://localhost:61616");
+        var fac=new org.apache.activemq.ActiveMQConnectionFactory("tcp://localhost:61616");
         var cnn= fac.createConnection();
-        var session= cnn.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        //第一个参数，true为开启事务，false不开启事物
+        var session= cnn.createSession(true, Session.AUTO_ACKNOWLEDGE);
         var msg= session.createTextMessage();
         msg.setText("hell world");
 
         var target= session.createQueue("udl");
         var producer= session.createProducer(target);
         producer.send(msg);
+        //事物session,消息send之后需要commit,场景：一个事情需要发送多个消息
+        session.commit();
+
         producer.close();
         session.close();
         cnn.close();
-
     }
 }
