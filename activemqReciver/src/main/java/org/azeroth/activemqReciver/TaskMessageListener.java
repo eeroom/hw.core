e6a533d04e38a3c8cfd4c39eb9e8d0720e7cb5e5
邓常青 <eedroom@163.com>
2021-07-03 23:24:19 +0800

    使用消息监听器接受消息

--- activemqReciver/src/main/java/org/azeroth/activemqReciver/TaskMessageListener.java
+++ activemqReciver/src/main/java/org/azeroth/activemqReciver/TaskMessageListener.java
@@ -6,6 +6,7 @@ import javax.jms.MessageListener;
 public class TaskMessageListener implements MessageListener {
     @Override
     public void onMessage(Message message) {
+        //这个方法不是被新起线程调用，必须等到这里代码全部执行完，消息监听器才会去获取下一个消息
         System.out.println("收到消息");
     }
 }
