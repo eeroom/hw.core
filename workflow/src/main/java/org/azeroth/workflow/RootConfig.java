187c1e89f177e129d3f9a9ee8c31fd93e9631832
eeroom <eedroom@163.com>
2021-09-02 10:40:04 +0800

    移除不用依赖

--- workflow/src/main/java/org/azeroth/workflow/RootConfig.java
+++ workflow/src/main/java/org/azeroth/workflow/RootConfig.java
@@ -12,14 +12,15 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@ComponentScan(excludeFilters ={@ComponentScan.Filter(value = EnableWebMvc.class)})
+@ComponentScan(excludeFilters ={@ComponentScan.Filter(value =EnableWebMvc.class)})
 @EnableSwagger2
 public class RootConfig {
 
     @Bean
     public Docket createRestApi() {
         //启用swagger,添加@EnableSwagger2,添加这个bean
-        //默认地址 http://localhost:8084/doc.html
+        //swagger-ui的默认地址 http://localhost:8084/doc.html
+        //这里配置的都是swagger,关键接口的地址默认是http://地址/v2/api-docs
         var ab= new ApiInfoBuilder()
                 .title("azeroth-api")
                 .description("swagger-bootstrap-ui")
