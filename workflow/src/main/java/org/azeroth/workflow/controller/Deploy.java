c0a77b0c51bed71335bcbc4f06af9cdb507ff398
eeroom <eedroom@163.com>
2021-09-15 18:34:27 +0800

    asp.net风格和swaggerui不适配场景（涉及上传文件的表单）的规避做法

--- workflow/src/main/java/org/azeroth/workflow/controller/Deploy.java
+++ workflow/src/main/java/org/azeroth/workflow/controller/Deploy.java
@@ -5,11 +5,10 @@ import org.azeroth.workflow.viewModel.DeployAdd;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @Scope(WebApplicationContext.SCOPE_REQUEST)
@@ -18,8 +17,10 @@ public class Deploy {
 
     ProcessEngine processEngine;
 
-    @HttpPost
-    public DeployAdd add(DeployAdd deployAdd){
+    //这里涉及文件和普通表单数据，asp.net风格的改动和swaage-ui配合的不好，所以使用PostMapping
+    //使用asp.net风格的做法，这里仍然ok,但是swagger-ui不能正常工作，
+    @PostMapping(path = "Deploy/add",consumes = MediaType.MULTIPART_FORM_DATA_VALUE,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public DeployAdd add(DeployAdd deployAdd, MultipartFile bpmnFile){
         return new DeployAdd();
     }
 }
