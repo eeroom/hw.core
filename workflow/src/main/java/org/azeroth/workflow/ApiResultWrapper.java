60c64987f25e988edf88b96b66b7b7937b31415d
eeroom <eedroom@163.com>
2021-09-18 12:03:26 +0800

    完善登陆

---
+++ workflow/src/main/java/org/azeroth/workflow/ApiResultWrapper.java
@@ -0,0 +1,44 @@
+package org.azeroth.workflow;
+
+public class ApiResultWrapper {
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    /**
+     * javax.servlet.http.HttpServletResponse.SC*****;
+     * @param code
+     */
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getTag() {
+        return tag;
+    }
+
+    public void setTag(Object tag) {
+        this.tag = tag;
+    }
+
+    Object data;
+    int code;
+    String message;
+    Object tag;
+}
--- workflow/src/main/java/org/azeroth/workflow/AuthenticationHandlerInterceptor.java
+++ workflow/src/main/java/org/azeroth/workflow/AuthenticationHandlerInterceptor.java
@@ -1,6 +1,10 @@
 package org.azeroth.workflow;
 
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -21,14 +25,14 @@ public class AuthenticationHandlerInterceptor implements HandlerInterceptor {
             return true;
         var jwtstring= httpServletRequest.getHeader("Authorization");
         if(jwtstring==null || jwtstring.length()<=0)
-            return false;
+            return this.noLogin(httpServletResponse);
         var jwtVerifier= com.auth0.jwt.JWT.require(Algorithm.HMAC256("hw@123456"))
                 .withIssuer("workflow")
                 .build();
         var jwtDecode= jwtVerifier.verify(jwtstring);
         var userName= jwtDecode.getClaim("userName").asString();
         if(userName==null ||userName.length()<=0)
-            return false;
+            return this.noLogin(httpServletResponse);
         var context= WebApplicationContextUtils.getWebApplicationContext(httpServletRequest.getServletContext());
         var loginUserInfo=context.getBean(LoginUserInfo.class);
         loginUserInfo.setName(userName);
@@ -44,4 +48,19 @@ public class AuthenticationHandlerInterceptor implements HandlerInterceptor {
     public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
 
     }
+
+    private boolean noLogin(HttpServletResponse httpServletResponse) throws Exception {
+        var apiresult=new ApiResultWrapper();
+        apiresult.setCode(HttpServletResponse.SC_FORBIDDEN);
+        apiresult.setMessage("请登陆");
+        com.fasterxml.jackson.databind.ObjectMapper objectMapper=new ObjectMapper();
+        //反序列的时候，如果json中有额外的字段，class没有与其对应的属性，默认报错，这样配置就不会报错，
+        //这个配置对于调用第三方接口特别重要，接口可能增加某个字段数据，但我们不需要用，也就没有修改代码
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
+        var rtbuffer= objectMapper.writeValueAsBytes(apiresult);
+        httpServletResponse.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        httpServletResponse.getOutputStream().write(rtbuffer);
+        return false;
+    }
+
 }
