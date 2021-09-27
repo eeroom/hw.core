66804d89e5056dd8f924d84dd88ebf79ac542b44
eeroom <eedroom@163.com>
2021-09-27 17:02:28 +0800

    分析springboot的关键配置点

--- springboot1x/src/main/java/org/azeroth/springboot1x/App.java
+++ springboot1x/src/main/java/org/azeroth/springboot1x/App.java
@@ -4,8 +4,11 @@ package org.azeroth.springboot1x;
 import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizerBeanPostProcessor;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.AbstractFilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -18,10 +21,15 @@ public class App {
         //整个程序的核心springcontext，SpringApplication.run方法最终会创建出这个context,并且调用refresh,这个是spring容器的常规操作
         //这个类重新onRefresh,加入私货createEmbeddedServletContainer，
         //createEmbeddedServletContainer方法会从context拿到EmbeddedServletContainerFactory的实例，这个bean的注册点就在EmbeddedServletContainerAutoConfiguration.EmbeddedTomcat
-        //然后调用EmbeddedServletContainerFactory的接口方法，并且传入一个回调ServletContextInitializer，让后续调用回到selfInitialize方法
-        //selfInitialize方法中，会传入servletcontext,然后把context存到servletcotnext中
+        //然后调用EmbeddedServletContainerFactory的接口方法，并且传入一个ServletContextInitializer的实例，让后续调用回到selfInitialize方法
+        //ServletContextInitializer就是servlet3.1以后的约定，tomcat是约定找classpath/META-INF/services/javax.servlet.ServletContrainerInitalizer中声明的类，实例化，然后调用接口，从而调用到我们的代码中
+        //TomcatEmbeddedServletContainerFactory的getEmbeddedServletContainer中是直接创建一个ServletContainerInitializer的实例TomcatStarter，然后添加到tomcat的context中
+        //ServletContextInitializer的实例被放到ServletContainerInitializer的实例TomcatStarter中，后续tomcat启动后就最终调回AnnotationConfigEmbeddedWebApplicationContext中的selfInitialize，
+        //selfInitialize方法中，会传入servletcontext,然后把springcontext存到servletcotnext中，把dispatchservlet添加到servletcontext中
         var contextClass= org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext.class;
-
+        var tomcatEmbeddedServletContainerFactory= org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory.class;
+        var servletContextInitializer= ServletContextInitializer.class;
+        var ServletContrainerInitalizer= javax.servlet.ServletContainerInitializer.class;
         //spring根据这里面定义的bean创建DispatcherServlet的实例，然后进行配置,内部的重要配置类：DispatcherServletRegistrationConfiguration，DispatcherServletConfiguration
         var dispatcherServletAutoConfiguration= DispatcherServletAutoConfiguration.class;
         //DispatcherServletConfiguration定义非常关键的bean;dispatchservlet的bean,后续tomcat利用servlet约定调用onstart都会涉及到他
@@ -35,10 +43,11 @@ public class App {
         var servletContainerConfigClass= EmbeddedServletContainerAutoConfiguration.class;
         //读取application.properties中的配置信息修改tomcat的实例
         var embcc= EmbeddedServletContainerCustomizerBeanPostProcessor.class;
-        //创建tomcat实例
+        //创建tomcat实例,添加ServletContainerInitializer，启动tomcat
         var embededTomcatClass= EmbeddedServletContainerAutoConfiguration.EmbeddedTomcat.class;
-
-
+        var tomcatEmbeddedServletContainerFactory2= org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory.class;
+        //initialize方法中启动tomcat
+        var tomcatEmbeddedServletContainer= TomcatEmbeddedServletContainer.class;
         org.springframework.boot.SpringApplication.run(App.class);
     }
 }
