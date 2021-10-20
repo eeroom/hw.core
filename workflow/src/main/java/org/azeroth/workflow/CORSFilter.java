2605d76eb0570fc12bf5c9edd5e92807550c376a
eeroom <eedroom@163.com>
2021-10-20 11:49:15 +0800

    登陆功能

--- workflow/src/main/java/org/azeroth/workflow/CORSFilter.java
+++ workflow/src/main/java/org/azeroth/workflow/CORSFilter.java
@@ -15,6 +15,7 @@ public class CORSFilter implements Filter {
         var request=(javax.servlet.http.HttpServletRequest)servletRequest;
         var response=(javax.servlet.http.HttpServletResponse)servletResponse;
         response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Expose-Headers","Authorization");
         if(!request.getMethod().equalsIgnoreCase("options")){
             filterChain.doFilter(servletRequest,servletResponse);
             return;
@@ -23,7 +24,6 @@ public class CORSFilter implements Filter {
         response.setHeader("Access-Control-Max-Age", "1728000");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"));
-        response.setHeader("Access-Control-Expose-Headers","Authorization");
         response.setStatus(HttpServletResponse.SC_OK);
     }
 
