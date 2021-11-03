f0c76215a58cdf8106b391e22320e819081c8fed
eeroom <eedroom@163.com>
2021-11-03 16:23:30 +0800

    完善登陆

--- workflow/src/main/java/org/azeroth/workflow/AuthenticationHandlerInterceptor.java
+++ workflow/src/main/java/org/azeroth/workflow/AuthenticationHandlerInterceptor.java
@@ -24,14 +24,14 @@ public class AuthenticationHandlerInterceptor implements HandlerInterceptor {
             return true;
         var jwtstring= httpServletRequest.getHeader("Authorization");
         if(jwtstring==null || jwtstring.length()<=0)
-            return this.noLogin(httpServletResponse);
+            return this.needLogin(httpServletResponse);
         var jwtVerifier= com.auth0.jwt.JWT.require(Algorithm.HMAC256("hw@123456"))
                 .withIssuer("workflow")
                 .build();
         var jwtDecode= jwtVerifier.verify(jwtstring);
         var userName= jwtDecode.getClaim("userName").asString();
         if(userName==null ||userName.length()<=0)
-            return this.noLogin(httpServletResponse);
+            return this.needLogin(httpServletResponse);
         var context= WebApplicationContextUtils.getWebApplicationContext(httpServletRequest.getServletContext());
         var loginUserInfo=context.getBean(LoginUserInfo.class);
         loginUserInfo.setName(userName);
@@ -48,10 +48,10 @@ public class AuthenticationHandlerInterceptor implements HandlerInterceptor {
 
     }
 
-    private boolean noLogin(HttpServletResponse httpServletResponse) throws Exception {
+    private boolean needLogin(HttpServletResponse httpServletResponse) throws Exception {
         var apiresult=new ApidataWrapper();
         apiresult.setCode(HttpServletResponse.SC_UNAUTHORIZED);
-        apiresult.setMessage("请登陆");
+        apiresult.setMessage("未登陆或者登陆已过期");
         com.fasterxml.jackson.databind.ObjectMapper objectMapper=new ObjectMapper();
         //反序列的时候，如果json中有额外的字段，class没有与其对应的属性，默认报错，这样配置就不会报错，
         //这个配置对于调用第三方接口特别重要，接口可能增加某个字段数据，但我们不需要用，也就没有修改代码
