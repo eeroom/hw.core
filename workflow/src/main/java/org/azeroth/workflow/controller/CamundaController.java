3da1997b52c604418686dd0d0f40e9c2d1866ddf
eeroom <eedroom@163.com>
2021-11-12 16:47:33 +0800

    获取指定人员名下的待处理任务

--- workflow/src/main/java/org/azeroth/workflow/controller/CamundaController.java
+++ workflow/src/main/java/org/azeroth/workflow/controller/CamundaController.java
@@ -1,23 +1,22 @@
 package org.azeroth.workflow.controller;
 
 import org.azeroth.workflow.HttpPost;
+import org.azeroth.workflow.LoginUserInfo;
 import org.azeroth.workflow.viewModel.DeployAdd;
 import org.azeroth.workflow.viewModel.StartProcessParameter;
-import org.camunda.bpm.engine.impl.persistence.entity.DeploymentEntity;
-import org.camunda.bpm.engine.repository.Deployment;
 import org.camunda.bpm.engine.repository.ProcessDefinition;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.DelegationState;
+import org.camunda.bpm.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collector;
 
 @RestController
 @Scope(WebApplicationContext.SCOPE_REQUEST)
@@ -25,6 +24,9 @@ public class CamundaController {
 
     @Autowired
     org.camunda.bpm.engine.ProcessEngine processEngine;
+
+    @Autowired
+    LoginUserInfo loginUserInfo;
 
     @HttpPost
     public DeployAdd deploy(DeployAdd deployAdd, MultipartFile bpmnFile) throws Throwable {
@@ -165,5 +167,179 @@ public class CamundaController {
         return tmp;
     }
 
+    public List<?> getUserTask(){
+        var lsttask= this.processEngine.getTaskService().createTaskQuery()
+                .taskAssignee(this.loginUserInfo.getName())
+                .list()
+                .stream()
+                .map(x->new Task() {
+                    @Override
+                    public String getId() {
+                        return x.getId();
+                    }
+
+                    @Override
+                    public String getName() {
+                        return  x.getName();
+                    }
+
+                    @Override
+                    public void setName(String s) {
+
+                    }
+
+                    @Override
+                    public String getDescription() {
+                        return  x.getDescription();
+                    }
+
+                    @Override
+                    public void setDescription(String s) {
+
+                    }
+
+                    @Override
+                    public int getPriority() {
+                        return  x.getPriority();
+                    }
+
+                    @Override
+                    public void setPriority(int i) {
+
+                    }
+
+                    @Override
+                    public String getOwner() {
+                        return  x.getOwner();
+                    }
+
+                    @Override
+                    public void setOwner(String s) {
+
+                    }
+
+                    @Override
+                    public String getAssignee() {
+                        return  x.getAssignee();
+                    }
+
+                    @Override
+                    public void setAssignee(String s) {
+
+                    }
+
+                    @Override
+                    public DelegationState getDelegationState() {
+                        return  x.getDelegationState();
+                    }
+
+                    @Override
+                    public void setDelegationState(DelegationState delegationState) {
+
+                    }
+
+                    @Override
+                    public String getProcessInstanceId() {
+                        return  x.getProcessInstanceId();
+                    }
+
+                    @Override
+                    public String getExecutionId() {
+                        return  x.getExecutionId();
+                    }
+
+                    @Override
+                    public String getProcessDefinitionId() {
+                        return x.getProcessDefinitionId();
+                    }
+
+                    @Override
+                    public String getCaseInstanceId() {
+                        return x.getCaseInstanceId();
+                    }
+
+                    @Override
+                    public void setCaseInstanceId(String s) {
+
+                    }
+
+                    @Override
+                    public String getCaseExecutionId() {
+                        return x.getCaseExecutionId();
+                    }
+
+                    @Override
+                    public String getCaseDefinitionId() {
+                        return x.getCaseDefinitionId();
+                    }
+
+                    @Override
+                    public Date getCreateTime() {
+                        return x.getCreateTime();
+                    }
+
+                    @Override
+                    public String getTaskDefinitionKey() {
+                        return x.getTaskDefinitionKey();
+                    }
+
+                    @Override
+                    public Date getDueDate() {
+                        return x.getDueDate();
+                    }
+
+                    @Override
+                    public void setDueDate(Date date) {
+
+                    }
+
+                    @Override
+                    public Date getFollowUpDate() {
+                        return x.getFollowUpDate();
+                    }
+
+                    @Override
+                    public void setFollowUpDate(Date date) {
+
+                    }
+
+                    @Override
+                    public void delegate(String s) {
+
+                    }
+
+                    @Override
+                    public void setParentTaskId(String s) {
+
+                    }
+
+                    @Override
+                    public String getParentTaskId() {
+                        return x.getId();
+                    }
+
+                    @Override
+                    public boolean isSuspended() {
+                        return x.isSuspended();
+                    }
+
+                    @Override
+                    public String getFormKey() {
+                        return x.getId();
+                    }
+
+                    @Override
+                    public String getTenantId() {
+                        return x.getId();
+                    }
+
+                    @Override
+                    public void setTenantId(String s) {
+
+                    }
+                })
+                .collect(java.util.stream.Collectors.toList());
+        return lsttask;
+    }
 
 }
