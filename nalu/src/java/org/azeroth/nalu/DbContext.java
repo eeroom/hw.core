3582b5734bc053524227a574e1938f7f59a4cab6
eeroom <eedroom@163.com>
2021-12-13 15:13:52 +0800

    解析查询sql的逻辑移到ParseSqlContext，便于实现其它类型数据库对应的解析逻辑

--- nalu/src/java/org/azeroth/nalu/DbContext.java
+++ nalu/src/java/org/azeroth/nalu/DbContext.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class DbContext {
-    protected String rowCountFiledName = "_rowsCount";
+
 
     ArrayList<MyFunction2Throwable<Connection,ParseSqlContext,Integer>> lstexecute=new ArrayList<>();
 
@@ -90,10 +90,12 @@ public abstract class DbContext {
 
     protected abstract Connection getConnection() throws Throwable;
 
+    protected abstract ParseSqlContext getParseSqlContext();
+
     protected  <T> PagingList<T> toList(MyFunction<ResultSet,T> map,MyAction2<ParseSqlContext,Boolean> preparecontex) throws Throwable {
-        ParseSqlContext context=new ParseSqlContext();
+        var context=this.getParseSqlContext();
         preparecontex.execute(context,true);
-        String cmdstr=this.parseSql(context);
+        String cmdstr=context.parseSql();
         var cnn= this.getConnection();
         var pst= cnn.prepareStatement(cmdstr);
         for (var i = 0; i<context.lstDbParameter.size(); i++){
@@ -107,68 +109,12 @@ public abstract class DbContext {
             if(!rs.next())
                 return rt;
             lst.add(map.apply(rs));
-            rt.count=rs.getInt(this.rowCountFiledName);
+            rt.count=rs.getInt(context.rowCountFiledName);
         }
         while (rs.next()){
             var obj= map.apply(rs);
             lst.add(obj);
         }
         return rt;
-    }
-
-    /**
-     * 适用于mssqlserver，其他数据库请重新实现该方法
-     * 这里的分页采用rownumber方式
-     * @param context
-     * @return
-     */
-   protected  String parseSql(ParseSqlContext context) {
-        var lstselect=context.lstSelectNode.stream().map(x->x.parse(context)).collect(java.util.stream.Collectors.toList());
-        String selectstr=String.join(",\r\n",lstselect);
-        String fromstr=String.format("%s as %s",context.fromTable.tableName,context.fromTable.tableAlias);
-        var joinstr="";
-        if(context.lstJoinNode.size()>0)
-            joinstr=String.join("\r\n",context.lstJoinNode.stream().map(x->x.parse(context)).collect(java.util.stream.Collectors.toList()));
-        String wherestr="";
-        if(context.whereNode!=null)
-            wherestr="where "+context.whereNode.parse(context);
-        String groupbystr="";
-        if(context.lstGroupByNode.size()>0)
-            groupbystr="group by "+String.join(",",context.lstGroupByNode.stream().map(x->x.parse(context)).collect(java.util.stream.Collectors.toList()));
-        String havingstr="";
-        if(context.havingNode!=null)
-            havingstr="having "+context.havingNode.parse(context);
-        String orderbystr="";
-        if(context.lstOrderByNode.size()>0)
-            orderbystr="order by "+String.join(",",context.lstOrderByNode.stream().map(x->x.parse(context)).collect(java.util.stream.Collectors.toList()));
-        if(context.takerows<1){//不分页
-            var sql=String.format("select %s \r\n from %s\r\n%s\r\n%s\r\n%s\r\n%s\r\n%s",
-                    selectstr,
-                    fromstr,
-                    joinstr,
-                    wherestr,
-                    groupbystr,
-                    havingstr,
-                    orderbystr);
-            return sql;
-        }else {
-            var tmpRowIndex = "_theRowIndex";
-            var cmdstr =String.format("select %s,\r\nROW_NUMBER() OVER(%s) AS %s \r\n from %s\r\n%s\r\n%s\r\n%s\r\n%s",
-                    selectstr,
-                    orderbystr,
-                    tmpRowIndex,
-                    fromstr,
-                    joinstr,
-                    wherestr,
-                    groupbystr,
-                    havingstr);
-            var sql = String.format("with htt AS(\r\n%s),\r\n hbb AS(\r\n select COUNT(0) AS %s from htt)\r\n select htt.*,hbb.* from htt,hbb WHERE htt.%s BETWEEN %s AND %s",
-                    cmdstr,
-                    this.rowCountFiledName,
-                    tmpRowIndex,
-                    String.valueOf(context.skiprows),
-                    String.valueOf(context.skiprows+context.takerows-1));
-            return sql;
-        }
     }
 }
--- nalu/src/java/org/azeroth/nalu/ParseSqlContext.java
+++ nalu/src/java/org/azeroth/nalu/ParseSqlContext.java
@@ -4,29 +4,94 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ParseSqlContext {
+
+    protected String rowCountFiledName = "_rowsCount";
    private int tableIndex=0;
-   int nextTableIndex(){
+    private int parameterIndex=0;
+    private int colindex=0;
+
+    protected int nextTableIndex(){
        return this.tableIndex++;
    }
+    protected int nextColIndex() {
+        return colindex++;
+    }
+    protected String nextParameterName(){
+        return "?";
+    }
    public ArrayList<DbSet> lstTable=new ArrayList<>();
    public WhereNode whereNode;
    public WhereNode havingNode;
    public DbSet fromTable;
    public ArrayList<SelectNode> lstSelectNode=new ArrayList<>();
-    private int colindex=0;
-    int nextColIndex() {
-        return colindex++;
-    }
-
     public ArrayList<JoinNode> lstJoinNode =new ArrayList<>();
 
-    private int parameterIndex=0;
-    protected String nextParameterName(){
-        return "?";
-    }
+
     public ArrayList<Tuple.Tuple2<String,Object>> lstDbParameter =new ArrayList<>();
     public ArrayList<OrderbyNode> lstOrderByNode=new ArrayList<>();
     public ArrayList<Column> lstGroupByNode=new ArrayList<>();
     public  int takerows;
     public  int skiprows;
+
+    protected String GetTableName(DbSet table){
+        return table.tableName;
+    }
+
+    protected String GetTableNameAlias(DbSet table){
+        return table.tableAlias;
+    }
+    /**
+     * 适用于mssqlserver，其他数据库请重新实现该方法
+     * 这里的分页采用rownumber方式
+     * @return
+     */
+    protected  String parseSql() {
+        var lstselect=this.lstSelectNode.stream().map(x->x.parse(this)).collect(java.util.stream.Collectors.toList());
+        String selectstr=String.join(",\r\n",lstselect);
+        String fromstr=String.format("%s as %s",this.GetTableName(this.fromTable),this.GetTableNameAlias(this.fromTable));
+        var joinstr="";
+        if(this.lstJoinNode.size()>0)
+            joinstr=String.join("\r\n",this.lstJoinNode.stream().map(x->x.parse(this)).collect(java.util.stream.Collectors.toList()));
+        String wherestr="";
+        if(this.whereNode!=null)
+            wherestr="where "+this.whereNode.parse(this);
+        String groupbystr="";
+        if(this.lstGroupByNode.size()>0)
+            groupbystr="group by "+String.join(",",this.lstGroupByNode.stream().map(x->x.parse(this)).collect(java.util.stream.Collectors.toList()));
+        String havingstr="";
+        if(this.havingNode!=null)
+            havingstr="having "+this.havingNode.parse(this);
+        String orderbystr="";
+        if(this.lstOrderByNode.size()>0)
+            orderbystr="order by "+String.join(",",this.lstOrderByNode.stream().map(x->x.parse(this)).collect(java.util.stream.Collectors.toList()));
+        if(this.takerows<1){//不分页
+            var sql=String.format("select %s \r\n from %s\r\n%s\r\n%s\r\n%s\r\n%s\r\n%s",
+                    selectstr,
+                    fromstr,
+                    joinstr,
+                    wherestr,
+                    groupbystr,
+                    havingstr,
+                    orderbystr);
+            return sql;
+        }else {
+            var tmpRowIndex = "_theRowIndex";
+            var cmdstr =String.format("select %s,\r\nROW_NUMBER() OVER(%s) AS %s \r\n from %s\r\n%s\r\n%s\r\n%s\r\n%s",
+                    selectstr,
+                    orderbystr,
+                    tmpRowIndex,
+                    fromstr,
+                    joinstr,
+                    wherestr,
+                    groupbystr,
+                    havingstr);
+            var sql = String.format("with htt AS(\r\n%s),\r\n hbb AS(\r\n select COUNT(0) AS %s from htt)\r\n select htt.*,hbb.* from htt,hbb WHERE htt.%s BETWEEN %s AND %s",
+                    cmdstr,
+                    this.rowCountFiledName,
+                    tmpRowIndex,
+                    String.valueOf(this.skiprows),
+                    String.valueOf(this.skiprows+this.takerows-1));
+            return sql;
+        }
+    }
 }
