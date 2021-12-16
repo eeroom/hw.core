e32c3f546cf42ba9e92167b552ca31005a012401
eeroom <eedroom@163.com>
2021-12-16 16:29:51 +0800

    完善Tuple类型

--- nalu/src/java/org/azeroth/nalu/Tuple.java
+++ nalu/src/java/org/azeroth/nalu/Tuple.java
@@ -7,9 +7,51 @@ public class Tuple {
         tmp.item2=b;
         return tmp;
     }
+    public static <T,B,X>  Tuple3<T,B,X> create(T t,B b,X x){
+        var tmp=new Tuple3<T,B,X>();
+        tmp.item1=t;
+        tmp.item2=b;
+        tmp.item3=x;
+        return tmp;
+    }
+    public static <T,B,X,Y>  Tuple4<T,B,X,Y> create(T t,B b,X x,Y y){
+        var tmp=new Tuple4<T,B,X,Y>();
+        tmp.item1=t;
+        tmp.item2=b;
+        tmp.item3=x;
+        tmp.item4=y;
+        return tmp;
+    }
+    public static <T,B,X,Y,Z>  Tuple5<T,B,X,Y,Z> create(T t,B b,X x,Y y,Z z){
+        var tmp=new Tuple5<T,B,X,Y,Z>();
+        tmp.item1=t;
+        tmp.item2=b;
+        tmp.item3=x;
+        tmp.item4=y;
+        tmp.item5=z;
+        return tmp;
+    }
 
     public static class Tuple2<T,B>{
        public T item1;
        public B item2;
+    }
+    public static class Tuple3<T,B,X>{
+        public T item1;
+        public B item2;
+        public X item3;
+    }
+    public static class Tuple4<T,B,X,Y>{
+        public T item1;
+        public B item2;
+        public X item3;
+        public Y item4;
+    }
+    public static class Tuple5<T,B,X,Y,Z>{
+        public T item1;
+        public B item2;
+        public X item3;
+        public Y item4;
+        public Z item5;
     }
 }
