fac011f6340b809988f6fa92f24823c2676c541b
eeroom <eedroom@163.com>
2021-12-22 11:28:00 +0800

    增加数据库对应的model

---
+++ entity/java/io/github/eeroom/entity/hzdb/bizsub.java
@@ -0,0 +1,75 @@
+package io.github.eeroom.entity.hzdb;
+
+public class bizsub {
+
+    Integer id;
+
+    public Integer getid() {
+        return id;
+    }
+
+    public void setid(Integer pValue) {
+        this.id = pValue;
+    }
+
+    String processId;
+
+    public String getprocessId() {
+        return processId;
+    }
+
+    public void setprocessId(String pValue) {
+        this.processId = pValue;
+    }
+
+    String taskId;
+
+    public String gettaskId() {
+        return taskId;
+    }
+
+    public void settaskId(String pValue) {
+        this.taskId = pValue;
+    }
+
+    String handlerId;
+
+    public String gethandlerId() {
+        return handlerId;
+    }
+
+    public void sethandlerId(String pValue) {
+        this.handlerId = pValue;
+    }
+
+    Integer result;
+
+    public Integer getresult() {
+        return result;
+    }
+
+    public void setresult(Integer pValue) {
+        this.result = pValue;
+    }
+
+    String completeformdatajson;
+
+    public String getcompleteformdatajson() {
+        return completeformdatajson;
+    }
+
+    public void setcompleteformdatajson(String pValue) {
+        this.completeformdatajson = pValue;
+    }
+
+    Integer taskstatus;
+
+    public Integer gettaskstatus() {
+        return taskstatus;
+    }
+
+    public void settaskstatus(Integer pValue) {
+        this.taskstatus = pValue;
+    }
+
+}
---
+++ entity/java/io/github/eeroom/entity/hzdb/jijianxinxi.java
@@ -0,0 +1,45 @@
+package io.github.eeroom.entity.hzdb;
+
+public class jijianxinxi {
+
+    String processId;
+
+    public String getprocessId() {
+        return processId;
+    }
+
+    public void setprocessId(String pValue) {
+        this.processId = pValue;
+    }
+
+    Integer sendCityId;
+
+    public Integer getsendCityId() {
+        return sendCityId;
+    }
+
+    public void setsendCityId(Integer pValue) {
+        this.sendCityId = pValue;
+    }
+
+    Integer reciceCityId;
+
+    public Integer getreciceCityId() {
+        return reciceCityId;
+    }
+
+    public void setreciceCityId(Integer pValue) {
+        this.reciceCityId = pValue;
+    }
+
+    Integer zongjia;
+
+    public Integer getzongjia() {
+        return zongjia;
+    }
+
+    public void setzongjia(Integer pValue) {
+        this.zongjia = pValue;
+    }
+
+}
