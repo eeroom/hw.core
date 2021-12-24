8439b0680fe79934438f164ef7044363e1503170
eeroom <eedroom@163.com>
2021-12-24 11:09:09 +0800

    国内快件流程v1.0

--- workflow-sf/java/io/github/eeroom/sf/bpm/BizdataStatusSetter.java
+++ workflow-sf/java/io/github/eeroom/sf/bpm/BizdataStatusSetter.java
@@ -3,20 +3,22 @@ package io.github.eeroom.sf.bpm;
 import io.github.eeroom.entity.sfdb.bizdata;
 import io.github.eeroom.sf.MyObjectFacotry;
 import io.github.eeroom.sf.SfDbContext;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.camunda.bpm.engine.delegate.ExecutionListener;
 import org.camunda.bpm.engine.delegate.TaskListener;
 
-public class BizdataStatusSetter implements TaskListener {
+public class BizdataStatusSetter implements ExecutionListener {
     @Override
-    public void notify(DelegateTask delegateTask) {
-        var pid= delegateTask.getProcessInstanceId();
+    public void notify(DelegateExecution delegateExecution) throws Exception {
+        var pid= delegateExecution.getProcessInstanceId();
         var dbcontext= MyObjectFacotry.getBean(SfDbContext.class);
         var bizd= dbcontext.dbSet(bizdata.class).select()
                 .where(x->x.col(a->a.getprocessId()).eq(pid))
                 .firstOrDefault();
         if(bizd==null)
             throw new RuntimeException("没有找到对应的bizdata,processId:"+pid);
-        var bizdatastatus=delegateTask.getVariable("bizdatastatus");
+        var bizdatastatus=delegateExecution.getVariable("bizdatastatus");
         var value= Integer.valueOf(bizdatastatus.toString());
         dbcontext.edit(bizdata.class)
                 .setUpdateCol(x->x.getStatus(),value)
