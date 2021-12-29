098c39f446f744f1b64d5430a1681d3b1f4de25d
eeroom <eedroom@163.com>
2021-12-29 14:53:49 +0800

    修改流程图

--- sf/java/io/github/eeroom/gtop/sf/camunda/GuoneiKuaidiHandler.java
+++ sf/java/io/github/eeroom/gtop/sf/camunda/GuoneiKuaidiHandler.java
@@ -3,10 +3,12 @@ package io.github.eeroom.gtop.sf.camunda;
 import io.github.eeroom.apiclient.HttpChannelFactory;
 import io.github.eeroom.gtop.api.sf.kuaidi.IGuoneiKuaidiCallback;
 import io.github.eeroom.gtop.entity.sf.db.kuaidientcustomer;
+import io.github.eeroom.gtop.entity.sf.kuaidi.EntityByCreate;
 import io.github.eeroom.gtop.entity.sf.kuaidi.FeedMessage;
 import io.github.eeroom.gtop.entity.sf.kuaidi.FeedType;
 import io.github.eeroom.gtop.sf.MyDbContext;
 import io.github.eeroom.gtop.sf.MyObjectFacotry;
+import io.github.eeroom.gtop.sf.serialize.JsonConvert;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -23,25 +25,24 @@ import java.util.HashMap;
 @Component
 public class GuoneiKuaidiHandler extends ListenerHandler implements Serializable {
 
-    public void feed(DelegateExecution delegateExecution) {
+    public void feed(DelegateExecution delegateExecution,String feedtype,String... msgkeys) {
         var pid= delegateExecution.getProcessInstanceId();
         var dbcontext= MyObjectFacotry.getBean(MyDbContext.class);
-        var thirdpartId=delegateExecution.getVariable("thirdpartId").toString();
-        var thridpart= dbcontext.dbSet(kuaidientcustomer.class)
+        var formdataOfCreate=delegateExecution.getVariable(VariableKey.formdataOfCreate).toString();
+        var enityByCreate= MyObjectFacotry.getBean(JsonConvert.class).deSerializeObject(formdataOfCreate, EntityByCreate.class);
+        var customer= dbcontext.dbSet(kuaidientcustomer.class)
                 .select()
-                .where(x->x.col(a->a.getid()).eq(thirdpartId))
+                .where(x->x.col(a->a.getid()).eq(enityByCreate.getCustomerId()))
                 .firstOrDefault();
-        if(thridpart==null)
-            throw new RuntimeException("指定的第三方不存在，info:"+thirdpartId);
+        if(customer==null)
+            throw new RuntimeException("指定的第三方不存在，info:"+enityByCreate.getCustomerId());
         //确定要发送的信息，流程图上指定
-        var msgkeys= (String)delegateExecution.getVariable("msgkeys");
-        var lstmsgkey=msgkeys.split(",");
-        HashMap<String,Object> data=new HashMap<>();
-        for (var key:lstmsgkey){
-            data.put(key,delegateExecution.getVariable(key));
+        HashMap<String,Object> map=new HashMap<>();
+        for (var key:msgkeys){
+            map.put(key,delegateExecution.getVariable(key));
         }
-        var msg=new FeedMessage(pid, FeedType.valueOf(delegateExecution.getVariable("msgtype").toString()),data);
-        var thirdpartCallbackHandler= HttpChannelFactory.createChannel(thridpart.getfeedbackurl(), IGuoneiKuaidiCallback.class);
-        var rt= thirdpartCallbackHandler.waitmsg(msg);
+        var msg=new FeedMessage(pid, FeedType.valueOf(feedtype),map);
+        var guoneiKuaidiCallback= HttpChannelFactory.createChannel(customer.getfeedbackurl(), IGuoneiKuaidiCallback.class);
+        var rt= guoneiKuaidiCallback.waitmsg(msg);
     }
 }
