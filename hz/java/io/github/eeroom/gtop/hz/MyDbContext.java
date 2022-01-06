72eb6830670a07967df3b511fefb2001b21543f9
eeroom <eedroom@163.com>
2022-01-06 16:10:47 +0800

    hz引入数据库连接池dbcp2

--- hz/java/io/github/eeroom/gtop/hz/MyDbContext.java
+++ hz/java/io/github/eeroom/gtop/hz/MyDbContext.java
@@ -3,7 +3,11 @@ package io.github.eeroom.gtop.hz;
 import io.github.eeroom.nalu.DbContext;
 import io.github.eeroom.nalu.ParseSqlContext;
 import io.github.eeroom.nalu.ParseSqlContextMysql;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
@@ -12,16 +16,22 @@ import java.sql.Connection;
 
 @Component
 @Scope(WebApplicationContext.SCOPE_REQUEST)
-public class MyDbContext extends DbContext {
+public class MyDbContext extends DbContext implements ApplicationContextAware {
     AppConfig config;
+    ApplicationContext applicationContext;
     public MyDbContext(AppConfig config){
         this.config=config;
     }
 
     @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext=applicationContext;
+    }
+    @Override
     protected Connection getConnection() throws Throwable {
         try {
-            return (Connection) MyObjectFacotry.getBean(RootConfig.basicDataSourceN1);
+            var ds= (BasicDataSource)this.applicationContext.getBean(RootConfig.basicDataSourceN1);
+            return  ds.getConnection();
         }catch (Throwable ex){
             throw new RuntimeException(String.format("获取数据库连接失败,url:%s,用户名：%s",this.config.dburln1,this.config.dbusernamen1),ex);
         }
@@ -31,5 +41,6 @@ public class MyDbContext extends DbContext {
     protected ParseSqlContext getParseSqlContext() {
         return new ParseSqlContextMysql();
     }
+
 
 }
