16ee89d19b3c1a0316665cdbeb4b21bc25679723
eeroom <eedroom@163.com>
2022-02-11 14:34:38 +0800

    jni

--- jni/java/io/github/eeroom/jni/App.java
+++ jni/java/io/github/eeroom/jni/App.java
@@ -1,12 +1,5 @@
 package io.github.eeroom.jni;
 
-import com.sun.jna.Function;
-import com.sun.jna.Native;
-import com.sun.jna.platform.win32.Guid;
-import com.sun.jna.platform.win32.Kernel32;
-import com.sun.jna.platform.win32.Kernel32Util;
-import com.sun.jna.win32.W32APIOptions;
-
 /**
  * Hello world!
  *
@@ -18,7 +11,7 @@ public class App
         //调用Azeorth.Core的Aclib项目类库，把lib放在resource目录就好，运行的时候，dll和jar在同一目录
         //dllname不要后缀，windows默认加.dll，linux默认加.so。也可以写全路径
         //64为jdk需要c类库也是按64位编译
-        var iaclib=Native.load(IAclib.dllName,IAclib.class);
+        var iaclib=com.sun.jna.Native.load(IAclib.dllName,IAclib.class);
         var rt= iaclib.Add(3,4);
         var rt2= iaclib.Handler("ha", new IAcHandlerCallback() {
             @Override
@@ -27,12 +20,7 @@ public class App
             }
         });
         //调用win32 api,W32APIOptions.ASCII_OPTIONS这个参数很关键
-        var k32= Native.load("Kernel32", Kernel32.class, W32APIOptions.ASCII_OPTIONS);
-        k32.LoadLibraryEx(IQiz.dllName,null,0);
-        var qzmodule= k32.GetModuleHandle(IQiz.dllName);
-        var pa= k32.GetProcAddress(qzmodule,3);//第三个函数就是CreateObject
-        var fcreateObject= Function.getFunction(pa,63);
-        fcreateObject.invoke(new Object[]{Guid.GUID.fromString("").getPointer()});
+        var k32= com.sun.jna.Native.load("Kernel32", com.sun.jna.platform.win32.Kernel32.class, com.sun.jna.win32.W32APIOptions.ASCII_OPTIONS);
 
         System.out.println( "Hello World!" );
     }
--- jni/java/io/github/eeroom/jni/IQiz.java
+++
@@ -1,9 +0,0 @@
-package io.github.eeroom.jni;
-
-import com.sun.jna.Pointer;
-
-
-public interface IQiz {
-    final String dllName="d:\03tools\7z\7z.dll";
-    void CreateObject(Pointer cid, Pointer iid, Pointer qzobj);
-}
