fdaf959076940ba3f9224ee4a91c5b5cfc5d3b95
eeroom <eedroom@163.com>
2022-06-30 14:47:21 +0800

    正则表达式

--- javacore/java/io/github/eeroom/javacore/text/App.java
+++ javacore/java/io/github/eeroom/javacore/text/App.java
@@ -1,4 +1,16 @@
 package io.github.eeroom.javacore.text;
 
 public class App {
+    public void regexMatch(){
+        var origin="<div>我的</div>QQ是:12345 我<a>的电</img>话是:7777 我<style>的邮</script>箱是:ss@163.com";
+        String[] holdTags={"a","img"};
+        var regexstr= String.format("<(?!((/?\\s?%s)))[^>]+>",String.join("\\b)|(/?\\s?",holdTags));
+        java.util.regex.Pattern p=java.util.regex.Pattern.compile(regexstr);
+        var m=p.matcher(origin);
+        while (m.find()){
+            System.out.println(m.group());
+            origin= origin.replace(m.group(),"");
+        }
+        System.out.println(origin);
+    }
 }
--- springcore/java/io/github/eeroom/springcore/Main.java
+++ springcore/java/io/github/eeroom/springcore/Main.java
@@ -100,17 +100,6 @@ public class Main {
         var sl=java.util.ServiceLoader.load(ServletContainerInitializer.class);
         sl.findFirst().get().onStartup(new Object());
 
-        var origin="<div>我的</div>QQ是:12345 我<a>的电</img>话是:7777 我<style>的邮</script>箱是:ss@163.com";
-        String[] holdTags={"a","img"};
-        var regexstr= String.format("<(?!((/?\\s?%s)))[^>]+>",String.join("\\b)|(/?\\s?",holdTags));
-        java.util.regex.Pattern p=java.util.regex.Pattern.compile(regexstr);
-        var m=p.matcher(origin);
-        while (m.find()){
-
-            System.out.println(m.group());
-            origin= origin.replace(m.group(),"");
-        }
-        System.out.println(origin);
 
     }
 }
