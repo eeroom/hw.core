42e7f50d0daa69168b96e19fefe40d3c631cc2a0
eeroom <eedroom@163.com>
2023-10-10 14:25:11 +0800

    jni移到javacore中

--- jni/java/io/github/eeroom/jni/IAcHandlerCallback.java
+++
@@ -1,7 +0,0 @@
-package io.github.eeroom.jni;
-
-import com.sun.jna.win32.StdCallLibrary;
-
-public interface IAcHandlerCallback extends StdCallLibrary.StdCallCallback {
-    int Add(int p1,int p2);
-}
--- jni/java/io/github/eeroom/jni/IAclib.java
+++
@@ -1,9 +0,0 @@
-package io.github.eeroom.jni;
-
-import com.sun.jna.Library;
-
-public interface IAclib extends Library {
-    final String dllName="aclib";
-    int Add(int p1,int p2);
-    int Handler(String p1,IAcHandlerCallback iAcHandlerCallback);
-}
