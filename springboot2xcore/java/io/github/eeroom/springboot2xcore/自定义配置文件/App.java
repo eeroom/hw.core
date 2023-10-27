65a974ea3a43f1bf4ad4e6bafb766f6ed1cdbb0f
eeroom <eedroom@163.com>
2023-10-27 16:16:32 +0800

    自定义配置文件

--- springboot2xcore/java/io/github/eeroom/springboot2xcore/自定义配置文件/App.java
+++ springboot2xcore/java/io/github/eeroom/springboot2xcore/自定义配置文件/App.java
@@ -14,14 +14,16 @@ public class App {
         //中文会乱码，所以额外处理
         var encodedResource=new EncodedResource(context.getResource(ApplicationContext.CLASSPATH_URL_PREFIX+"web.config"),"utf-8");
         var propertySource=new ResourcePropertySource(encodedResource);
-        context.registerBean(WebConfigProperty.class,beanDefinition->beanDefinition.setScope(ConfigurableBeanFactory.SCOPE_PROTOTYPE));
-
-        ConfigurationPropertiesBindingPostProcessor.register(context.getDefaultListableBeanFactory());
-        context.refresh();
         //如果对应的bean是单例，则必须在 context.refresh()之前添加，否则@ConfigurationProperties(prefix = "wch") 标注的bean 取不到这个resource中配置信息
         //因为context中执行了提前实例化单例bean
         //如果对应的bean是多例，则只要在获取bean之前添加，都能正常获取到配置值
         context.getEnvironment().getPropertySources().addLast(propertySource);
+
+        context.registerBean(WebConfigProperty.class,beanDefinition->beanDefinition.setScope(ConfigurableBeanFactory.SCOPE_PROTOTYPE));
+        //增加@ConfigurationProperties 注解配套的 bean后处理起，这样WebConfigProperty的字段才能正确的从配置文件中获取对应的值
+        ConfigurationPropertiesBindingPostProcessor.register(context.getDefaultListableBeanFactory());
+        context.refresh();
+
 
         var webconfig= context.getBean(WebConfigProperty.class);
         System.out.println("wch.name="+webconfig.name);
