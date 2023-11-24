5d1a784361ac693b2abf436a45709fcf4df39a78
eeroom <eedroom@163.com>
2023-11-24 11:11:41 +0800

    cloud-config

--- cloud-config/java/io/github/eeroom/cloudconfig/App.java
+++ cloud-config/java/io/github/eeroom/cloudconfig/App.java
@@ -3,14 +3,18 @@ package io.github.eeroom.cloudconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class App 
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(App.class,args);
+        var context= SpringApplication.run(App.class,args);
+        var password= context.getEnvironment().getProperty("spring.cloud.config.server.git.password");
+        System.out.println("当前密码："+password);
     }
 }
